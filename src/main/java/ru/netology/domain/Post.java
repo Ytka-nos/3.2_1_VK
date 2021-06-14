51340349db8e109ab7e4a361a0eb99ce9f308e46
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-06-14 21:41:33 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private String authorName;
+     private int authorAvatarUrl;
+     private String DateOfPost;
+     private String TextOfPost;
+     private String ImageUrl;
+     private String AwayUrl;
+     private Boolean saveToBookmarks;
+     private int CountOfLikes;
+     private int CountOfComments;
+     private int CountOfSharing;
+     private int CountOfWatching;
+
+    // + getters/setters
+
+
+
+
+}
